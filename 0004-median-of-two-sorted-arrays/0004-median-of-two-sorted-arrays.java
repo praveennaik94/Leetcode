729b630892b2ee9c85a6417e5f56c87078b4927d
Praveen Naik <naikp3256@gmail.com>
2026-07-09 11:58:29 +0530

    Time: 1 ms (100%), Space: 49.1 MB (15.81%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.java
@@ -1,35 +1,50 @@
 class Solution {
     public double findMedianSortedArrays(int[] arr1, int[] arr2) {
-        int n = arr1.length+arr2.length;
+        int n = arr1.length + arr2.length;
         
-        int ans[] = new int[n];
+        int idx1 = n / 2;
+        int idx2 = n / 2 - 1;
+        
+        int ele1 = -1;
+        int ele2 = -1;
         
         int i = 0;
         int j = 0;
-        int idx = 0;
-        while(i < arr1.length && j < arr2.length){
-            if(arr1[i] <= arr2[j]){
-                ans[idx] = arr1[i];
-                idx++; i++;
-            }else{
-                ans[idx] = arr2[j];
-                idx++;j++; 
+        int count = 0;
+        
+        while (i < arr1.length && j < arr2.length) {
+        
+            if (arr1[i] <= arr2[j]) {
+                if (count == idx1) ele1 = arr1[i];
+                if (count == idx2) ele2 = arr1[i];
+                count++;
+                i++;
+            } else {
+                if (count == idx1) ele1 = arr2[j];
+                if (count == idx2) ele2 = arr2[j];
+                count++;
+                j++;
             }
         }
         
-        while(i < arr1.length){
-            ans[idx] = arr1[i];
-            idx++; i++;
+        while (i < arr1.length) {
+            if (count == idx1) ele1 = arr1[i];
+            if (count == idx2) ele2 = arr1[i];
+            count++;
+            i++;
         }
         
-        while(j < arr2.length){
-            ans[idx] = arr2[j];
-            idx++; j++;
+        while (j < arr2.length) {
+            if (count == idx1) ele1 = arr2[j];
+            if (count == idx2) ele2 = arr2[j];
+            count++;
+            j++;
         }
         
-        if(n % 2 == 0)
-            return (double) (ans[(n/2)-1] + ans[n/2])/2;
-        
-        return (double) ans[n/2];
+        if (n % 2 == 0) {
+            return (ele1 + ele2) / 2.0;
+        } 
+
+        return ele1;
     }
 }
