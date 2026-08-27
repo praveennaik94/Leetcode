1f0a329c4c962eabb7a1a97efd3dda0f47914958
Praveen Naik <naikp3256@gmail.com>
2026-08-27 22:54:31 +0530

    Time: 3 ms (86.07%), Space: 42.8 MB (98.31%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean isValid(String s) {
+        if (s.length() <= 1) return false;
+
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            
+            char ch = s.charAt(i);
+
+            if (ch == '(' || ch == '[' || ch == '{') {
+                stack.push(ch);
+            } 
+            else {
+                
+                if (stack.isEmpty()) return false;
+
+                if (ch == ')' && stack.peek() != '(') return false;
+                if (ch == ']' && stack.peek() != '[') return false;
+                if (ch == '}' && stack.peek() != '{') return false;
+
+                stack.pop();
+            }
+        }
+
+        return stack.isEmpty();
+    }
+}
