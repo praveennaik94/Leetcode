b6e871fb5e71cb9343dff1f13e30685c435d694c
Praveen Naik <naikp3256@gmail.com>
2026-09-01 18:15:21 +0530

    Time: 44 ms (16.33%), Space: 46.4 MB (64.26%) - LeetHub

--- 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -1,20 +1,93 @@
 class Solution {
     public long subArrayRanges(int[] arr) {
-        long sum = 0;
-        
-        for(int i = 0; i < arr.length; i++){
-            
-            long min = Integer.MAX_VALUE;
-            long max = Integer.MIN_VALUE;
-            
-            for(int j = i; j < arr.length; j++){
-                min = Math.min(arr[j], min);
-                max = Math.max(arr[j], max);
-                
-                sum += max - min;
+        return sumSubarrayMaxs(arr) - sumSubarrayMins(arr);
+    }
+    private int[] findNSE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] >= arr[i]) {
+                st.pop();
             }
+            ans[i] = !st.isEmpty() ? st.peek() : n;
+            st.push(i);
         }
-        
+        return ans;
+    }
+
+    // Function to find indices of Next Greater Elements
+    private int[] findNGE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!st.isEmpty() && arr[st.peek()] <= arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : n;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    // Function to find indices of Previous Smaller or Equal Elements
+    private int[] findPSEE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] > arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    // Function to find indices of Previous Greater or Equal Elements
+    private int[] findPGEE(int[] arr) {
+        int n = arr.length;
+        int[] ans = new int[n];
+        Stack<Integer> st = new Stack<>();
+        for (int i = 0; i < n; i++) {
+            while (!st.isEmpty() && arr[st.peek()] < arr[i]) {
+                st.pop();
+            }
+            ans[i] = !st.isEmpty() ? st.peek() : -1;
+            st.push(i);
+        }
+        return ans;
+    }
+
+    // Function to compute sum of subarray minimums
+    private long sumSubarrayMins(int[] arr) {
+        int n = arr.length;
+        int[] nse = findNSE(arr);
+        int[] psee = findPSEE(arr);
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i - psee[i];
+            int right = nse[i] - i;
+            long freq = 1L * left * right;
+            sum += freq * arr[i];
+        }
+        return sum;
+    }
+
+    // Function to compute sum of subarray maximums
+    private long sumSubarrayMaxs(int[] arr) {
+        int n = arr.length;
+        int[] nge = findNGE(arr);
+        int[] pgee = findPGEE(arr);
+        long sum = 0;
+        for (int i = 0; i < n; i++) {
+            int left = i - pgee[i];
+            int right = nge[i] - i;
+            long freq = 1L * left * right;
+            sum += freq * arr[i];
+        }
         return sum;
     }
 }
