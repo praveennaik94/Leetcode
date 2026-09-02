3097caa60264677d21c9e85b6241d39e622f67d9
Praveen Naik <naikp3256@gmail.com>
2026-09-02 12:08:50 +0530

    Time: 2 ms (97.63%), Space: 47 MB (56.43%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.java
+++ 0735-asteroid-collision/0735-asteroid-collision.java
@@ -1,35 +1,24 @@
 class Solution {
     public int[] asteroidCollision(int[] arr) {
-        Stack<Integer> stack = new Stack<>();
+        ArrayList<Integer> list = new ArrayList<>(); 
         
         for(int i = 0; i < arr.length; i++){
-            if(arr[i] > 0) stack.push(arr[i]);
+            if(arr[i] > 0) list.add(arr[i]);
             else{
                 
-                while(!stack.isEmpty() && stack.peek() > 0 && Math.abs(arr[i]) > stack.peek()) stack.pop();
+                while(!list.isEmpty() && list.get(list.size() -1 ) > 0 && Math.abs(arr[i]) > list.get(list.size() -1 )) list.remove(list.size()-1);
                 
-                if(!stack.isEmpty() && stack.peek() == Math.abs(arr[i])) {
-                    stack.pop();
+                if(!list.isEmpty() && list.get(list.size()-1) == Math.abs(arr[i])) {
+                    list.remove(list.size()-1);
                 }
-                else if(stack.isEmpty() || stack.peek() < 0) stack.push(arr[i]);
+                else if(list.isEmpty() || list.get(list.size()-1) < 0) list.add(arr[i]);
             }
         }
 
-        if(stack.isEmpty()) return new int[]{};
+        if(list.isEmpty()) return new int[]{};
 
-        int[] ans = new int[stack.size()];
-        int idx = 0;
-        while(!stack.isEmpty()){
-            ans[idx++] = stack.pop();
-        }
-        
-        int i = 0; int j = ans.length-1;
-        while(i < j){
-            int temp = ans[i];
-            ans[i] = ans[j];
-            ans[j] = temp;
-            i++; j--;
-        }
+        int[] ans = new int[list.size()];
+        for(int i = 0; i < ans.length; i++) ans[i] = list.get(i);
         
         return ans;
     }
